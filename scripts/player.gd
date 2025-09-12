8c7c8a0d647e496830d4e962bb3000ccba756e55
IIIap <krab.khazhin@gmail.com>
2025-09-13 00:49:19 +0300

    MY GOD

---
+++ scripts/player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+@export var speed = 200
+@export var backward_multiplier = 0.7
+@export var health = 6.0
+@export var turret_timer = 0.4
+@export var can_shoot = true
+@export var alive = true
+
+var player_projectile_scene = preload("res://scenes/playerProjectile.tscn")
+
+func _on_turret_timer_timeout() -> void:
+	can_shoot = true
+
+func _ready() -> void:
+	pass
+	
+func turret():
+	$Turret.look_at(get_global_mouse_position())
+func control(delta: float):
+	var rotation_input = 0.0
+	if Input.is_action_pressed("right"):
+		rotation_input += 2
+	elif Input.is_action_pressed("left"):
+		rotation_input -= 2
+	
+	if Input.is_action_pressed("forward"):
+		velocity = Vector2(speed, 0).rotated(rotation)
+	elif Input.is_action_pressed("back"):
+		velocity = Vector2(-speed*backward_multiplier, 0).rotated(rotation)
+	else:
+		velocity = Vector2.ZERO
+	rotation += rotation_input * delta
+func shoot():
+	
+	if Input.is_action_just_pressed("shoot"):
+		if can_shoot:
+			can_shoot = false
+			$TurretTimer.start(turret_timer)
+			var projectile_instance = player_projectile_scene.instantiate()
+			projectile_instance.position = $Turret/Marker.global_position
+			projectile_instance.rotation = rotation + $Turret.rotation
+			get_tree().get_root().add_child(projectile_instance)
+			
+
+func _process(delta: float) -> void:
+	turret()
+	control(delta)
+	shoot()
+	move_and_slide()
---
+++ scripts/projectile.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+@export var speed: float = 600
+@export var damage: float = 1
+@export var lifeTime: float = 2.00
+
+func explode():
+	queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	explode()
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+		
+func _on_life_timer_timeout() -> void:
+	explode()
+
+func _ready() -> void:
+	$LifeTimer.wait_time = lifeTime
+	$LifeTimer.start()
+	
+func _process(delta: float) -> void:
+	position += transform.x * speed * delta
