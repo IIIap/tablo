5fb104fcba24a5a1fb7bc88da65ff94893a877e9
IIIap <krab.khazhin@gmail.com>
2025-09-16 21:46:42 +0300

    работаю над ломанием уровня

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -3,6 +3,7 @@ extends Area2D
 @export var speed: float = 600
 @export var damage: float = 1
 @export var lifeTime: float = 2.00
+signal get_cell_vector(cell_vector: Vector2)
 
 func explode():
 	speed = 0
@@ -15,7 +16,9 @@ func _on_body_entered(body: Node2D) -> void:
 	explode()
 	if body.has_method("take_damage"):
 		body.take_damage(damage)
-		
+	
+	if body is TileMapLayer:
+		emit_signal("get_cell_vector", body.local_to_map(global_position))
 func _on_life_timer_timeout() -> void:
 	explode()
 
